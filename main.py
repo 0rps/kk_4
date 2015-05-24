6fd231c7ab4036f5248cd9b1bf498d8d9056024f
Alex <tuxrps@gmail.com>
2015-05-24 15:27:06 +0300

    sec

--- main.py
+++ main.py
@@ -20,20 +20,17 @@ class Parser:
 		def internalParse(expr):
 			res = self.parse(expr)
 
-			# hardcode: checking is current parser is var parser
-			# and if next char in chain is '=' then print result[0]
-			flag = self.isvar and res[1][0] == '='
-
 			if res is not None:
 				otherRes = other.parse(res[1])
+
 				if otherRes is None:
 					return None
 
 				res[0].extend(otherRes[0])
 				res[1] = otherRes[1]
 
-				if flag:
-					varsResult.append(reduce(lambda x, y: "{0} {1}", res[0], ''))
+				if other.isvar:
+					varsResult.append(reduce(lambda x, y: "{0} {1}".format(x, y), res[0], ''))
 
 				return res
 
@@ -70,10 +67,9 @@ def wordParser(letter):
 	return baseParser(lambda c: c == letter)
 
 
-def regexParser(regex, isvar=False):
+def regexParser(regex):
 	cregex = re.compile('^{0}$'.format(regex))
 	parser = baseParser(lambda expr: cregex.match(expr) is not None)
-	parser.isvar = isvar
 
 	return parser
 
@@ -84,6 +80,16 @@ def manyWordParser(*args):
 
 	return res
 
+def saveVarParser():
+	def internalParse(expr):
+		return [[], expr]
+
+	parser = Parser(internalParse)
+	parser.isvar = True
+
+	return parser
+
+
 if __name__ == '__main__':
 
 	relationOperation = manyWordParser('<', '<=', '==', '<>', '>', '>=')
@@ -91,18 +97,27 @@ if __name__ == '__main__':
 	multiplyOperation = manyWordParser('*', '/')
 
 	arithmeticExpr = wordParser('non_alter')
-	term = wordParser('non_alter')
-	operatorsList = wordParser('non_alter')
+	arithmeticExpr_dash = wordParser('non_alter')
+	operatorsList_dash = wordParser('non_alter')
+	term_dash = wordParser('non_alter')
 
 	for i in range(0, 50):
 		factor = regexParser('[a-z]+') | regexParser('[0-9]+') | wordParser('(') >> arithmeticExpr >> wordParser(')')
-		term = term >> multiplyOperation >> factor | factor
-		arithmeticExpr = arithmeticExpr >> sumOperation >> term | term
+		term_dash = multiplyOperation >> factor >> term_dash | multiplyOperation >> factor
+		term = factor >> term_dash | factor
+		arithmeticExpr_dash = sumOperation >> term | sumOperation >> term >> arithmeticExpr_dash
+		arithmeticExpr = term >> arithmeticExpr_dash | term
 		expr = arithmeticExpr >> relationOperation >> arithmeticExpr | arithmeticExpr
-		operator = regexParser('[a-z]+', True) >> wordParser('=') >> expr
-		operatorsList = operator | operatorsList >> wordParser(';') >> operator
+		operator = regexParser('[a-z]+') >> wordParser('=') >> expr >> saveVarParser()
+		operatorsList_dash = wordParser(';') >> operator | wordParser(';') >> operator >> operatorsList_dash
+		operatorsList = operator >> operatorsList_dash | operator
 		block = wordParser('begin') >> operatorsList >> wordParser('end')
 		s = block
 
-	chain = '1234 * 1234 * var * 134'
-	print term.parse(chain.split(' '))
+	chain = 'begin arg = 1234 * 1234 * var * 134 ; vav = 134 * 12 end'
+	#chain = ''
+	chain = chain.split(' ')
+	print s.parse(chain)
+	print ''
+	for line in varsResult:
+		print line
